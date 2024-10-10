df1c1bb4754899037bcc79520df2b80d506f385d
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-10-10 09:24:58 +0530

    Update LandingPage styles

--- src/Pages/landingPage.js
+++ src/Pages/landingPage.js
@@ -27,7 +27,7 @@ const LandingPage = () => {
       <Header/>
       <div className="h-full bg-gray-100 flex flex-col justify-around py-6">
         <div className="flex  flex-col items-center justify-center">
-          <h1 className="text-5xl font-bold">Welcome to EduTech+</h1>
+          <h1 className="text-5xl font-bold text-center">Welcome to EduTech+</h1>
           <p className="mt-4 text-lg">Your pathway to knowledge and success.</p>
           <button className="mt-6 px-4 py-2 bg-blue-600 text-white rounded" onClick={handleGetStarted}>Get Started</button>
         </div>
